2d27c09d184ffc0c02b4a39dd533a86ac76c3fde
Gianni Fabriziani <gianni.fabriziani@gmail.com>
2020-02-26 10:29:14 +0100

    add color format selector from materialui

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import Select from '@material-ui/core/Select';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
+import { MenuItem } from '@material-ui/core';
 
 export default class Navbar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            format: 'hex'
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(e) {
+        this.setState({ format: e.target.value });
+        this.props.handleChange(e.target.value);
+    }
+
     render() {
         const { level, changeLevel } = this.props;
+        const { format } = this.state;
         return (
-            <div className="Navbar">
+            <header className="Navbar">
                 <div className="logo">
                     <a href="#">reactcolorpicker</a>
                 </div>
@@ -23,7 +39,16 @@ export default class Navbar extends Component {
                         />
                     </div>
                 </div>
-            </div>
+                <div className="select-container">
+                    <Select value={format} onChange={this.handleChange}>
+                        <MenuItem value="hex">HEX - #ffffff</MenuItem>
+                        <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+                        <MenuItem value="rgba">
+                            RGBA - rgba(255,255,255,1.0)
+                        </MenuItem>
+                    </Select>
+                </div>
+            </header>
         );
     }
 }
