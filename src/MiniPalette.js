540e02231d30b8dab508271ac6697c1bd764db65
Gianni Fabriziani <gianni.fabriziani@gmail.com>
2020-02-27 11:00:59 +0100

    finish styling minipalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -13,7 +13,13 @@ const styles = {
             cursor: 'pointer'
         }
     },
-    colors: { backgroundColor: 'gray' },
+    colors: {
+        backgroundColor: '#dae1e4',
+        height: '150px',
+        width: '100%',
+        borderRadius: '5px',
+        overflow: 'hidden'
+    },
     title: {
         display: 'flex',
         justifyContent: 'space-between',
@@ -25,15 +31,30 @@ const styles = {
         position: 'relative'
     },
 
-    emoji: { marginLeft: '0.5rem', fontSize: '1.5rem' }
+    emoji: { marginLeft: '0.5rem', fontSize: '1.5rem' },
+    miniColors: {
+        height: '25%',
+        width: '20%',
+        display: 'inline-block',
+        margin: '0 auto',
+        position: 'relative',
+        marginBottom: '-3.5px'
+    }
 };
 
 function MiniPalette(props) {
-    const { classes, paletteName, emoji } = props;
+    const { classes, paletteName, emoji, colors } = props;
+    const miniColorBoxes = colors.map(color => (
+        <div
+            className={classes.miniColors}
+            style={{ backgroundColor: color.color }}
+            key={color.name}
+        />
+    ));
     console.log(classes);
     return (
         <div className={classes.root}>
-            <div className={classes.colors}></div>
+            <div className={classes.colors}>{miniColorBoxes}</div>
             <h5 className={classes.title}>
                 {paletteName}
                 <span className={classes.emoji}>{emoji}</span>
