6786ccf5ba4e0709a424531bcd88e6d7e9000bbd
Gianni Fabriziani <gianni.fabriziani@gmail.com>
2020-02-27 11:29:49 +0100

    ad link to palette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -28,6 +28,15 @@ const styles = {
     }
 };
 class PaletteList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this.goToPalette = this.goToPalette.bind(this);
+    }
+
+    goToPalette(id) {
+        this.props.history.push(`/palette/${id}`);
+    }
     render() {
         const { palettes, classes } = this.props;
         return (
@@ -38,7 +47,10 @@ class PaletteList extends Component {
                     </nav>
                     <div className={classes.palettes}>
                         {palettes.map(palette => (
-                            <MiniPalette {...palette} />
+                            <MiniPalette
+                                {...palette}
+                                handleClick={() => this.goToPalette(palette.id)}
+                            />
                         ))}
                     </div>
                 </div>
