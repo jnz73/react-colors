503209da7c403b27d5cd84a96bce275974b84dfa
Gianni Fabriziani <gianni.fabriziani@gmail.com>
2020-03-02 11:26:58 +0100

    display colors in SingleColorPalette

--- src/App.js
+++ src/App.js
@@ -34,7 +34,14 @@ function App() {
             />
             <Route
                 path="/palette/:paletteId/:colorId"
-                render={() => <SingleColorPalette />}
+                render={routeProps => (
+                    <SingleColorPalette
+                        colorId={routeProps.match.params.colorId}
+                        palette={generatePalette(
+                            findPalette(routeProps.match.params.paletteId)
+                        )}
+                    />
+                )}
             />
         </Switch>
     );
