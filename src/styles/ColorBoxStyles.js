ef5faeab1a0f02a390965732877fb8ee71ec4066
Gianni Fabriziani <gianni.fabriziani@gmail.com>
2020-03-25 15:43:32 +0100

    make ColorBox responsive

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -15,13 +15,13 @@ export default {
         },
         [sizes.down('lg')]: {
             width: '25%',
-            height: props => (props.showingFullPalette ? '20%' : '50%')
+            height: props => (props.showingFullPalette ? '20%' : '33.33333%')
         },
         [sizes.down('md')]: {
             width: '50%',
-            height: props => (props.showingFullPalette ? '10%' : '50%')
+            height: props => (props.showingFullPalette ? '10%' : '20%')
         },
-        [sizes.down('sm')]: {
+        [sizes.down('xs')]: {
             width: '100%',
             height: props => (props.showingFullPalette ? '5%' : '10%')
         }
@@ -120,7 +120,8 @@ export default {
             textAlign: 'center',
             marginBottom: '0',
             padding: '1rem',
-            textTransform: 'uppercase'
+            textTransform: 'uppercase',
+            [sizes.down('xs')]: { fontSize: '6rem' }
         },
         '& p': {
             fontSize: '2rem',
